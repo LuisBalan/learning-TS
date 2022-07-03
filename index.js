261d74271130caaf49eee3d23bd59499d4ee38ba
LuisBalan <jlbalcz@gmail.com>
2022-07-03 12:16:59 -0500

    add first lesson exercises

--- index.js
+++ index.js
@@ -1,36 +1,30 @@
 console.log("Hello, world!");
-
 //Type Inferences
-let aged = true;
-let realAge = 0;
-
-if(aged){
+var aged = true;
+var realAge = 0;
+if (aged) {
     realAge = 4;
 }
-
-let dogAge = realAge * 7;
-console.log(`${dogAge} years`);
-
+var dogAge = realAge * 7;
+console.log(dogAge + " years");
 //Type Shapes
-let firstName = "muriel!";
-
+var firstName = "muriel!";
 console.log(firstName.toUpperCase());
 console.log(firstName.length);
-
 //Any
 /*
 We can declare a variable without initialize. TS will recognize it as any-type variable */
-let guess;
+var guess;
 guess = "color";
 console.log(guess);
 guess = 10;
 console.log(guess);
-
 //Variable type annotations
-let phoneNumber : string;
-if(Math.random() > 0.5){
+var phoneNumber;
+if (Math.random() > 0.5) {
     phoneNumber = '+61770102062';
-}else{
+}
+else {
     phoneNumber = '71677623223';
 }
-console.log(phoneNumber)
+console.log(phoneNumber);
